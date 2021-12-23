4a0b8cce2a88d0584606d53e468760a52d2028cf
saranavarros <mariasaranavarro@super.ufam.edu.br>
2021-12-23 16:15:55 -0400

    add user's fpga interfaces responsivity

--- remotelab/src/rotas/general.js
+++ remotelab/src/rotas/general.js
@@ -11,4 +11,10 @@ module.exports = (app) => {
     app.get('/user-clp', (req, res) =>{
         res.render("lab-clp/user-clp")
     });
+    app.get('/user-fpga', (req, res) => {
+        res.render("lab-fpga/user-fpga")
+    });
+    app.get('/Trail-intro-fpga', (req, res) => {
+        res.render("lab-fpga/Trail-intro-fpga")
+    });
 }
