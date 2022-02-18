bbebcd5dd3da25892d508251d8b95556480366a9
Caroline Soares Braz <48094755+carolsbraz@users.noreply.github.com>
2022-02-18 07:52:06 -0400

    update routes

--- src/routes/general-routes.js
+++ src/routes/general-routes.js
@@ -15,4 +15,38 @@ module.exports = (app) => {
     app.get('/crud-user', (req, res) =>{
         res.render("general/crud-user")
     });
+
+     // fpga 
+
+     app.get('/program', (req, res) =>{
+        res.render("lab-fpga/program-fpga")
+    });
+    app.get('/user-fpga', (req, res) =>{
+        res.render("lab-fpga/user-fpga")
+    });
+    app.get('/Trail-intro-fpga', (req, res) =>{
+        res.render("lab-fpga/Trail-intro-fpga")
+    });
+
+    // clp
+
+    app.get('/user-clp', (req, res) =>{
+        res.render("lab-clp/user-clp")
+    });
+
+    app.get('/clp-crud', (req, res) =>{
+        res.render("lab-clp/clp-crud")
+    });
+    
+    app.get('/clp-data', (req, res) =>{
+        res.render("lab-clp/clp-data")
+    });
+
+    app.get('/new-clp-version', (req, res) =>{
+        res.render("lab-clp/new-clp-version")
+    });
+
+    app.get('/clp-diagram', (req, res) =>{
+        res.render("lab-clp/diagram")
+    });
 }
--- src/server.js
+++ src/server.js
@@ -8,8 +8,6 @@ server.set('view engine', 'ejs')
 server.set('views', './src/views');
 
 require(__dirname + "/routes/general-routes.js")(server);
-require(__dirname + "/routes/clp-routes.js")(server);
-require(__dirname + "/routes/fpga-routes.js")(server);
 
 //ligar o servidor
 http.createServer(server).listen(process.env.PORT || 3000, () => console.log("Servidor rodando"));
